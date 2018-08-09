bb467904ee524aca7a53022597b60d44aa6183ab
markstepchin <markstepchin@gmail.com>
2018-08-09 11:42:47 -0700

    seperated header into seperate component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <!-- header --> */}
-        <header class='container-fluid py-4'>
-          <div class="container">
-            <div class="row">
-              <div class='col'>
-                <img src="./img/logo.svg" alt="coverhound logo" height="46px" width="auto" />
-              </div>
-              <div class='col float-right d-none d-md-block'>
-                <p class='text-right white-text'>Have Questions?</p>
-                <p class='text-right green-text'>Call us at (844) 278-3674</p>
-              </div>
-              <div class='col float-right d-block d-md-none d-lg-none d-xl-none'>
-                <button class='btn float-right' type="button">
-                  <i class="fas fa-phone u-color-white fa-flip-horizontal"></i>
-                </button>
-
-              </div>
-            </div>
-          </div>
-        </header>
-        {/* <!-- /header --> */}
+        <Header />
 
 
 
