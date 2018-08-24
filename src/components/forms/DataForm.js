f6ccda65486e77932d02ae7ddb865c7b635213bc
markstepchin <markstepchin@gmail.com>
2018-08-24 00:47:02 -0700

    refactored the dataform component so that it uses the redux state instead of component state

--- src/components/forms/DataForm.js
+++ src/components/forms/DataForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CheckBoxInput from './utilities/CheckBoxInput';
+import { customEvent } from './utilities/customEvent';
 import CheckBoxGroup from './utilities/CheckBoxGroup';
 import ButtonGroup from './utilities/ButtonGroup';
 import NextButton from '../layout/NextButton';
@@ -9,32 +10,9 @@ import NextButton from '../layout/NextButton';
 import { updateField } from '../../ducks/fields';
 
 class DataForm extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      backupData: null,
-      backupOnSite: false,
-      backupOffSitePhysical: false,
-      backupOffSiteCloud: false,
-      systemIntrustion: false,
-      tampering: false,
-      codeAttacks: false,
-      lossOfData: false,
-      lossOfMedia: false,
-      hackingIncident: false,
-      extortionAttempt: false,
-      dataTheft: false,
-      copyrightDispute: false,
-      dataLoss: false,
-      incomeLoss: false,
-      legalThreat: false,
-      actualInsuranceClaim: false,
-      suspectedInsuranceClaim: false
-    };
-  }
-
-  onFieldChange = ({ target: { value, name } }) => {
-    this.props.dispatch(updateField({ business_info: { [name]: value } }));
+  onFieldChange = ({ target: { value, name, checked, type } }) => {
+    const newValue = type === 'checkbox' ? checked : value;
+    this.props.dispatch(updateField({ business_info: { [name]: newValue } }));
   };
 
   render() {
@@ -50,70 +28,31 @@ class DataForm extends React.Component {
           }
         >
           <CheckBoxInput
+            name="encrypt_on_computers"
             label="Office Computers"
             checked={this.props.business_info.encrypt_on_computers || false}
-            unClick={() => {
-              const event = {};
-              event.target = {};
-              event.target.name = 'encrypt_on_computers';
-              event.target.value = false;
-
-              this.onFieldChange(event);
-            }}
-            onChange={() => {
-              const prevValue = this.props.business_info.encrypt_on_computers || false;
-
-              const event = {};
-              event.target = {};
-              event.target.name = 'encrypt_on_computers';
-              event.target.value = !prevValue;
-
-              this.onFieldChange(event);
-            }}
+            unClick={() =>
+              this.onFieldChange(customEvent({ name: 'encrypt_on_computers', value: false }))
+            }
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="encrypt_on_mobile"
             label="Mobile Devices (laptops, cell phones, flash drives, tablets, etc.)"
             checked={this.props.business_info.encrypt_on_mobile || false}
-            unClick={() => {
-              const event = {};
-              event.target = {};
-              event.target.name = 'encrypt_on_mobile';
-              event.target.value = false;
-
-              this.onFieldChange(event);
-            }}
-            onChange={() => {
-              const prevValue = this.props.business_info.encrypt_on_mobile || false;
-
-              const event = {};
-              event.target = {};
-              event.target.name = 'encrypt_on_mobile';
-              event.target.value = !prevValue;
-
-              this.onFieldChange(event);
-            }}
+            unClick={() =>
+              this.onFieldChange(customEvent({ name: 'encrypt_on_mobile', value: false }))
+            }
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="encrypt_on_networks"
             label="Networks"
             checked={this.props.business_info.encrypt_on_networks || false}
-            unClick={() => {
-              const event = {};
-              event.target = {};
-              event.target.name = 'encrypt_on_networks';
-              event.target.value = false;
-
-              this.onFieldChange(event);
-            }}
-            onChange={() => {
-              const prevValue = this.props.business_info.encrypt_on_networks || false;
-
-              const event = {};
-              event.target = {};
-              event.target.name = 'encrypt_on_networks';
-              event.target.value = !prevValue;
-
-              this.onFieldChange(event);
-            }}
+            unClick={() =>
+              this.onFieldChange(customEvent({ name: 'encrypt_on_networks', value: false }))
+            }
+            onChange={this.onFieldChange}
           />
         </CheckBoxGroup>
 
@@ -124,128 +63,116 @@ class DataForm extends React.Component {
               ? null
               : this.props.business_info.backup_data
           }
-          yesClick={() => {
-            const event = {};
-            event.target = {};
-            event.target.name = 'backup_data';
-            event.target.value = true;
-            this.onFieldChange(event);
-          }}
-          noClick={() => {
-            const event = {};
-            event.target = {};
-            event.target.name = 'backup_data';
-            event.target.value = false;
-            this.onFieldChange(event);
-          }}
+          yesClick={() => this.onFieldChange(customEvent({ name: 'backup_data', value: true }))}
+          noClick={() => this.onFieldChange(customEvent({ name: 'backup_data', value: false }))}
         />
 
         <CheckBoxGroup label="Where do you back it up? (select all that apply)" noneOption={false}>
           <CheckBoxInput
+            name="backup_on_site"
             label="On-site"
-            checked={this.state.backupOnSite}
-            onChange={() =>
-              this.setState(prevState => ({
-                backupOnSite: !prevState.backupOnSite
-              }))
-            }
+            checked={this.props.business_info.backup_on_site || false}
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="backup_on_offsite"
             label="Offsite (physical storage)"
-            checked={this.state.backupOffSitePhysical}
-            onChange={() =>
-              this.setState(prevState => ({
-                backupOffSitePhysical: !prevState.backupOffSitePhysical
-              }))
-            }
+            checked={this.props.business_info.backup_on_offsite || false}
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="backup_on_cloud"
             label="Offsite (the Cloud)"
-            checked={this.state.backupOffSiteCloud}
-            onChange={() =>
-              this.setState(prevState => ({
-                backupOffSiteCloud: !prevState.backupOffSiteCloud
-              }))
-            }
+            checked={this.props.business_info.backup_on_cloud || false}
+            onChange={this.onFieldChange}
           />
         </CheckBoxGroup>
 
         <CheckBoxGroup
           label="Within the last 5 years, have you experienced or have reason to suspect any of the following (or something similar to them):"
           noneOption={true}
+          childrenNull={
+            typeof this.props.business_info.system_intrustion === 'undefined' &&
+            typeof this.props.business_info.tampering === 'undefined' &&
+            typeof this.props.business_info.code_attacks === 'undefined' &&
+            typeof this.props.business_info.loss_of_data === 'undefined' &&
+            typeof this.props.business_info.loss_of_media === 'undefined' &&
+            typeof this.props.business_info.hacking_incident === 'undefined' &&
+            typeof this.props.business_info.extortion_attempt === 'undefined' &&
+            typeof this.props.business_info.data_theft === 'undefined' &&
+            typeof this.props.business_info.copyright_dispute === 'undefined'
+          }
         >
           <CheckBoxInput
+            name="system_intrustion"
             label="System intrusions"
-            checked={this.state.systemIntrustion}
-            onChange={() =>
-              this.setState(prevState => ({
-                systemIntrustion: !prevState.systemIntrustion
-              }))
+            checked={this.props.business_info.system_intrustion || false}
+            unClick={() =>
+              this.onFieldChange(customEvent({ name: 'system_intrustion', value: false }))
             }
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="tampering"
             label="Tampering"
-            checked={this.state.tampering}
-            onChange={() => this.setState(prevState => ({ tampering: !prevState.tampering }))}
+            checked={this.props.business_info.tampering || false}
+            unClick={() => this.onFieldChange(customEvent({ name: 'tampering', value: false }))}
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="code_attacks"
             label="Virus or malicious code attacks"
-            checked={this.state.codeAttacks}
-            onChange={() =>
-              this.setState(prevState => ({
-                codeAttacks: !prevState.codeAttacks
-              }))
-            }
+            checked={this.props.business_info.code_attacks || false}
+            unClick={() => this.onFieldChange(customEvent({ name: 'code_attacks', value: false }))}
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="loss_of_data"
             label="Loss of Data"
-            checked={this.state.lossOfData}
-            onChange={() =>
-              this.setState(prevState => ({
-                lossOfData: !prevState.lossOfData
-              }))
-            }
+            checked={this.props.business_info.loss_of_data || false}
+            unClick={() => this.onFieldChange(customEvent({ name: 'loss_of_data', value: false }))}
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="loss_of_media"
             label="Loss of portable media"
-            checked={this.state.lossOfMedia}
-            onChange={() =>
-              this.setState(prevState => ({
-                lossOfMedia: !prevState.lossOfMedia
-              }))
-            }
+            checked={this.props.business_info.loss_of_media || false}
+            unClick={() => this.onFieldChange(customEvent({ name: 'loss_of_media', value: false }))}
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="hacking_incident"
             label="Hacking incident"
-            checked={this.state.hackingIncident}
-            onChange={() =>
-              this.setState(prevState => ({
-                hackingIncident: !prevState.hackingIncident
-              }))
+            checked={this.props.business_info.hacking_incident || false}
+            unClick={() =>
+              this.onFieldChange(customEvent({ name: 'hacking_incident', value: false }))
             }
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="extortion_attempt"
             label="Extortion attempt"
-            checked={this.state.extortionAttempt}
-            onChange={() =>
-              this.setState(prevState => ({
-                extortionAttempt: !prevState.extortionAttempt
-              }))
+            checked={this.props.business_info.extortion_attempt || false}
+            unClick={() =>
+              this.onFieldChange(customEvent({ name: 'extortion_attempt', value: false }))
             }
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="data_theft"
             label="Data theft"
-            checked={this.state.dataTheft}
-            onChange={() => this.setState(prevState => ({ dataTheft: !prevState.dataTheft }))}
+            checked={this.props.business_info.data_theft || false}
+            unClick={() => this.onFieldChange(customEvent({ name: 'data_theft', value: false }))}
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="copyright_dispute"
             label="Copyright or trademark dispute"
-            checked={this.state.copyrightDispute}
-            onChange={() =>
-              this.setState(prevState => ({
-                copyrightDispute: !prevState.copyrightDispute
-              }))
+            checked={this.props.business_info.copyright_dispute || false}
+            unClick={() =>
+              this.onFieldChange(customEvent({ name: 'copyright_dispute', value: false }))
             }
+            onChange={this.onFieldChange}
           />
         </CheckBoxGroup>
 
@@ -253,47 +180,52 @@ class DataForm extends React.Component {
           label="Did the incident(s) described above result in any of the following (or would they have if you had cyber insurance at the
                         time)?:"
           noneOption={true}
+          childrenNull={
+            typeof this.props.business_info.data_loss === 'undefined' &&
+            typeof this.props.business_info.income_loss === 'undefined' &&
+            typeof this.props.business_info.legal_threat === 'undefined' &&
+            typeof this.props.business_info.actual_insurance_claim === 'undefined' &&
+            typeof this.props.business_info.suspected_insurance_claim === 'undefined'
+          }
         >
           <CheckBoxInput
+            name="data_loss"
             label="Loss of sensitive data"
-            checked={this.state.dataLoss}
-            onChange={() => this.setState(prevState => ({ dataLoss: !prevState.dataLoss }))}
+            checked={this.props.business_info.data_loss || false}
+            unClick={() => this.onFieldChange(customEvent({ name: 'data_loss', value: false }))}
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="income_loss"
             label="Loss of revenue or income"
-            checked={this.state.incomeLoss}
-            onChange={() =>
-              this.setState(prevState => ({
-                incomeLoss: !prevState.incomeLoss
-              }))
-            }
+            checked={this.props.business_info.income_loss || false}
+            unClick={() => this.onFieldChange(customEvent({ name: 'income_loss', value: false }))}
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="legal_threat"
             label="Threatened or actual legal action"
-            checked={this.state.legalThreat}
-            onChange={() =>
-              this.setState(prevState => ({
-                legalThreat: !prevState.legalThreat
-              }))
-            }
+            checked={this.props.business_info.legal_threat || false}
+            unClick={() => this.onFieldChange(customEvent({ name: 'legal_threat', value: false }))}
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="actual_insurance_claim"
             label="Actual insurance claim"
-            checked={this.state.actualInsuranceClaim}
-            onChange={() =>
-              this.setState(prevState => ({
-                actualInsuranceClaim: !prevState.actualInsuranceClaim
-              }))
+            checked={this.props.business_info.actual_insurance_claim || false}
+            unClick={() =>
+              this.onFieldChange(customEvent({ name: 'actual_insurance_claim', value: false }))
             }
+            onChange={this.onFieldChange}
           />
           <CheckBoxInput
+            name="suspected_insurance_claim"
             label="Suspected insurance claim"
-            checked={this.state.suspectedInsuranceClaim}
-            onChange={() =>
-              this.setState(prevState => ({
-                suspectedInsuranceClaim: !prevState.suspectedInsuranceClaim
-              }))
+            checked={this.props.business_info.suspected_insurance_claim || false}
+            unClick={() =>
+              this.onFieldChange(customEvent({ name: 'suspected_insurance_claim', value: false }))
             }
+            onChange={this.onFieldChange}
           />
         </CheckBoxGroup>
 
